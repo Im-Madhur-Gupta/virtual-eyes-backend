b8e1cdf0ddcbf47af779b5319503fb913fadbfde
Madhur Gupta <madhurgupta107@gmail.com>
2022-05-17 17:41:00 +0530

    deploy fix 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const visualizeImageRouter = require("./src/routes/visualizeImage.routes");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
